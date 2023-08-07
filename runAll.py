19b3c5080afa3e48925d3851ede9d9a600ce1a2d
xenera-lisongyuan <xenera-lisongyuan@xenera.jp>
2023-08-07 15:25:22 +0800

    add 5 files on 2023871530

--- runAll.py
+++ runAll.py
@@ -2,25 +2,13 @@ import os
 import pytest
 import time
 
-# -------Below are debug os.system("python ./test_pytest.py")-------
-# os.system("python ./1test_ui_chrome1.py")
-# os.system("python ./test_api_get2.py")
-# os.system("python ./1test_api_post1.py")
-# os.system("python ./1test_api_post2.py")
-# os.system("python ./test_pytest4.py")
-# os.system("python ./1test_pytest2.py")
-
 if __name__ == '__main__':
-    """Please use this file to start all the test cases. Then the results will be published with Allure Report"""
+    """Please use this main function to start all the test cases. Then the results will be published with Allure Report"""
+    os.system("pytest --alluredir=./report  --clean-alluredir")
     pytest.main()
-    # time.sleep(2)
+    time.sleep(2)
+    os.system("allure generate report -o report\\allure-report -c report\\allure-report")
+    os.system("allure open report\\allure-report")
 
-    # os.system("pytest --alluredir=./report  --clean-alluredir")
-    # os.system("pytest 1test_pytest2.py -vs --alluredir=./report")
-    # # os.system("pytest 1test_pytest2.py -vs --alluredir=./report")
-    # # os.system("pytest 1test_pytest3.py -vs --alluredir=./report")
-    # # os.system("pytest test_pytest4.py -vs --alluredir=./report")
-    # # # os.system("pytest test_pytest4.py -vs --alluredir=./report  --clean-alluredir")
-    # # # os.system("copy environment.properties report\\")
-    # os.system("allure generate report -o report\\allure-report -c report\\allure-report")
-    # os.system("allure open report\\allure-report")
+    """Please use this main function to start all the test cases. Then the results will be published with html Report"""
+    # pytest.main()
--- test_api_post1.py
+++ test_api_post1.py
@@ -9,19 +9,17 @@ class Test_Api1():
 
     @allure.feature("test_api_post1")
     def test_post1(self):
-        user_info = {
-        "token": "129n8yexqy3om8ryec"
-        }
         s = requests.Session()
-        r = s.post("https://result.eolink.com/vk17wiAd3a7119b4f476f76f44ddf904c0bdbc6aa6ae800?uri=/user/login_json.php", user_info)
+        r = s.post("https://result.eolink.com/vk17wiAd3a7119b4f476f76f44ddf904c0bdbc6aa6ae800?uri=/user/login_json.php")
         pprint(r.json())
         for key, value in r.json().items():
             if key == "status":
                 assert value == "success"
+            elif key == "user_info":
+                for key1, value1 in value.items():
+                    assert value1 == "129n8yexqy3om8ryec"
 
 if __name__ == '__main__':
     test_api1 = Test_Api1()
     test_api1.test_post1()
-    time.sleep(2)
-    os.system("allure generate ./report -o")
-
+    # time.sleep(2)
